006a55039604821170e7b17d07b89ea08eed26e9
jenkinuser <snp@gmail.com>
2023-04-02 10:47:50 +0530

    first commit

---
+++ Greater.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Greater {
+    public static void main(String[] args) {
+        System.out.println("what is your name ?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("hello " + name + "!!");
+
+    }
+}
