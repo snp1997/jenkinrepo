bd893239805f1a1d2fa4ee068abcc8b1ba969cda
jenkinuser <snp@gmail.com>
2023-04-02 20:22:19 +0530

    second commit

--- Greater.java
+++ Greater.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Greater {
     public static void main(String[] args) {
-        System.out.println("what is your name ?");
+        System.out.println("what is your name please?");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
 
